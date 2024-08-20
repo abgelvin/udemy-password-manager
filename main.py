66ac9d7fafb550e1ce0b55ddddcb6c7a04694645
abgelvin <60162973+abgelvin@users.noreply.github.com>
2024-08-20 15:26:57 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title('Password Manager')
+
+canvas = Canvas(width=200, height=200)
+photo = PhotoImage(file='photo.png')
+canvas.create_image(20, 20)
